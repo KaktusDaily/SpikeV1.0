60748a193bd3465132b674f398b55ce4e862354e
KaktusDaily <115354717+KaktusDaily@users.noreply.github.com>
2026-02-25 13:27:24 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create virtual environment
+python -m venv chatbot_env
+source chatbot_env/bin/activate  # On Windows: chatbot_env\Scripts\activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the chatbot
+python llm_chatbot.py
